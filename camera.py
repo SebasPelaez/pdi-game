64cb1bcf96771df76dca1aed71abbea363e1f4bb
Sebas Pelaez <sebaspvilla@hotmail.com>
2020-03-23 01:13:03 -0500

    Se puede pintar con varios colores y varias brochas

--- camera.py
+++ camera.py
@@ -15,33 +15,46 @@ def delimit_screen(frame):
 
 def _paint_menu(window,colors):
 
-	colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)]
-	colorIndex = 0
-
 	# Setup the Paint interface
 	window = cv2.line(window, (150,0), (150,500), (0,0,0), 4)
-	window = cv2.rectangle(window, (10,5), (140,50), colors[0], -1)
-	window = cv2.rectangle(window, (10,60), (140,105), colors[1], -1)
-	window = cv2.rectangle(window, (10,115), (140,160), colors[2], -1)
-	window = cv2.rectangle(window, (10,170), (140,215), colors[3], -1)
+	window = cv2.rectangle(window, (10,68), (140,108), colors[0], -1)
+	window = cv2.rectangle(window, (10,115), (140,155), colors[1], -1)
+	window = cv2.rectangle(window, (10,162), (140,202), colors[2], -1)
+	window = cv2.rectangle(window, (10,209), (140,249), colors[3], -1)
 
-	window = cv2.circle(window, (75,245), 10, (0,0,0), -1)
-	window = cv2.circle(window, (75,285), 20, (0,0,0), -1)
-	window = cv2.circle(window, (75,345), 30, (0,0,0), -1)
-	window = cv2.circle(window, (75,425), 40, (0,0,0), -1)
+	window = cv2.circle(window, (75,275), 10, (0,0,0), -1)
+	window = cv2.circle(window, (75,315), 20, (0,0,0), -1)
+	window = cv2.circle(window, (75,375), 30, (0,0,0), -1)
 
-	cv2.putText(window, "AZUL", (55, 35), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
-	cv2.putText(window, "VERDE", (53, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
-	cv2.putText(window, "ROJO", (55, 145), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
-	cv2.putText(window, "AMARILLO", (40, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (150,150,150), 2, cv2.LINE_AA)
+	cv2.putText(window, "AZUL", (55, 92), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+	cv2.putText(window, "VERDE", (53, 142), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+	cv2.putText(window, "ROJO", (55, 190), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+	cv2.putText(window, "AMARILLO", (40, 235), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (150,150,150), 2, cv2.LINE_AA)
+
+	return window
+
+def _paint_rules(window,colors,idx,brushes,brush_idx):
+
+	cv2.putText(window, "Parate sobre el", (19, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 0), 1, cv2.LINE_AA)
+	cv2.putText(window, "color o la brocha", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 0), 1, cv2.LINE_AA)
+	cv2.putText(window, "que desees", (35, 45), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 0), 1, cv2.LINE_AA)
+
+	cv2.putText(window, "Color Seleccionado", (160, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+	window = cv2.rectangle(window, (312,6), (350,26), colors[idx], -1)
+	cv2.putText(window, "Brocha Seleccionada", (360, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+	window = cv2.rectangle(window, (525,6), (550,26), (255,255,255), -1)
+	cv2.putText(window, str(brushes[brush_idx]), (530, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
 
 	return window
 
 colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)]
-colorIndex = 0
+brushes = [10,20,30]
+color_idx = 0
+brush_idx = 0
 
 paintWindow = np.zeros((471,636,3)) + 255
 paintWindow = _paint_menu(paintWindow,colors)
+paintWindow = _paint_rules(paintWindow,colors,color_idx,brushes,brush_idx)
 cv2.namedWindow('Paint', cv2.WINDOW_AUTOSIZE)
 
 cap = cv2.VideoCapture(0)
@@ -52,14 +65,40 @@ while(True):
    
     object_point_one, object_point_two, object_half_point = objectUtils.identify_object(frame)
 
-    if ((object_point_one and object_point_two) and
-     (object_point_one[0] >= MENU_LIMIT_AREA and object_point_two[0] >= MENU_LIMIT_AREA)):
+    if object_point_one and object_point_two:
 
-    	frame = cv2.rectangle(frame,object_point_one,object_point_two,(0, 255, 0),4)
-    	frame = cv2.circle(frame, object_half_point, 10, (0,255,0), -1)
-    	cv2.circle(paintWindow, object_half_point, 10, (0,255,0), -1)
+    	if object_point_one[0] >= MENU_LIMIT_AREA and object_point_two[0] >= MENU_LIMIT_AREA:
 
+    		frame = cv2.rectangle(frame,object_point_one,object_point_two,colors[color_idx],4)
+	    	frame = cv2.circle(frame, object_half_point, brushes[brush_idx], colors[color_idx], -1)
+	    	cv2.circle(paintWindow, object_half_point, brushes[brush_idx], colors[color_idx], -1)
+    	
+    	else:
+
+    		if object_half_point[0] >= 10 and object_half_point[0] <= 140:
+
+	    		if object_half_point[1] >= 68 and object_half_point[1] <= 108:
+	    			color_idx = 0
+	    		elif object_half_point[1] >= 115 and object_half_point[1] <= 155:
+	    			color_idx = 1
+	    		elif object_half_point[1] >= 162 and object_half_point[1] <= 202:
+	    			color_idx = 2
+	    		elif object_half_point[1] >= 209 and object_half_point[1] <= 249:
+	    			color_idx = 3
+
+	    	if object_half_point[0] >= 75 and object_half_point[0] <= 100:
+
+	    		if object_half_point[1] >= 275 and object_half_point[1] <= 295:
+	    			brush_idx = 0
+	    		elif object_half_point[1] >= 315 and object_half_point[1] <= 335:
+	    			brush_idx = 1
+	    		elif object_half_point[1] >= 375 and object_half_point[1] <= 395:
+	    			brush_idx = 2
+	       	
     frame = delimit_screen(frame)
+    frame = _paint_menu(frame,colors)
+    paintWindow = _paint_rules(paintWindow,colors,color_idx,brushes,brush_idx)
+
     cv2.imshow('frame',frame)
     cv2.imshow("Paint", paintWindow)
     if cv2.waitKey(20) & 0xFF == ord('q'):
