452bb7688c945e924b44bcb3c944fe0fa050f6b1
Sebas Pelaez <sebaspvilla@hotmail.com>
2020-03-22 23:04:43 -0500

    Se construye el menú

--- camera.py
+++ camera.py
@@ -13,6 +13,37 @@ def delimit_screen(frame):
 
 	return frame
 
+def _paint_menu(window,colors):
+
+	colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)]
+	colorIndex = 0
+
+	# Setup the Paint interface
+	window = cv2.line(window, (150,0), (150,500), (0,0,0), 4)
+	window = cv2.rectangle(window, (10,5), (140,50), colors[0], -1)
+	window = cv2.rectangle(window, (10,60), (140,105), colors[1], -1)
+	window = cv2.rectangle(window, (10,115), (140,160), colors[2], -1)
+	window = cv2.rectangle(window, (10,170), (140,215), colors[3], -1)
+
+	window = cv2.circle(window, (75,245), 10, (0,0,0), -1)
+	window = cv2.circle(window, (75,285), 20, (0,0,0), -1)
+	window = cv2.circle(window, (75,345), 30, (0,0,0), -1)
+	window = cv2.circle(window, (75,425), 40, (0,0,0), -1)
+
+	cv2.putText(window, "AZUL", (55, 35), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+	cv2.putText(window, "VERDE", (53, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+	cv2.putText(window, "ROJO", (55, 145), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+	cv2.putText(window, "AMARILLO", (40, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (150,150,150), 2, cv2.LINE_AA)
+
+	return window
+
+colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)]
+colorIndex = 0
+
+paintWindow = np.zeros((471,636,3)) + 255
+paintWindow = _paint_menu(paintWindow,colors)
+cv2.namedWindow('Paint', cv2.WINDOW_AUTOSIZE)
+
 cap = cv2.VideoCapture(0)
 
 while(True):
@@ -28,6 +59,7 @@ while(True):
 
     frame = delimit_screen(frame)
     cv2.imshow('frame',frame)
+    cv2.imshow("Paint", paintWindow)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
